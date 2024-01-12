2d3bd94ea84a203d44e5de002ae3edd47d228f83
Shashwat <shashwatcse6204@gmail.com>
2024-01-12 23:17:48 +0530

    packages updated and frontend layout done

---
+++ frontend/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Importing the main component of your app
+import './index.css'; // Importing your styles if needed
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
