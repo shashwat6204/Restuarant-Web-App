b47f2fba1f1c2cbf000c34875b6dcdfd33772593
Shashwat <shashwatcse6204@gmail.com>
2024-01-12 23:11:12 +0530

    Packages updated and navabar and footer made

---
+++ frontend/src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-primary text-white text-lg p-4 text-center">
+      <p>&copy; 2024 My Restaurant. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
